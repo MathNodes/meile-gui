3d46c8acc7f99a9fda1d74fb5e3c40d36ac25b7e
freQniK <freQniK@protonmail.com>
2022-06-23 00:15:41 -0400

    Resize card. Floatlayout on Node card.

--- tests/rv-stuff.py
+++ tests/rv-stuff.py
@@ -13,8 +13,8 @@ Builder.load_string('''
     canvas:
         Rectangle:
             texture: self.texture
-            size: self.width-45 , self.height +30
-            pos: self.x - 150, self.y - 30             
+            size: self.width, self.height 
+                         
 <-FullImage2>:
     canvas:
         Rectangle:
@@ -24,76 +24,67 @@ Builder.load_string('''
                 
 <RecycleViewRow>:
     size_hint_y: None
-    size: "180dp", "180dp"
     pos_hint: {"center_x": .5, "center_y": .5}
     orientation: "vertical"
     padding: 10
     border_radius: 20
-    radius: [15]
     elevation:0
     md_bg_color: get_color_from_hex("#0d021b")
     MDGridLayout:
-        cols: 3
-        rows: 2
-        padding: 40,0,0,0
-        MDLabel:
-            text: "Schrodinger's Cat"
-            theme_text_color: "Custom"
-            font_style: "H6"
-            font_size: "24sp"
-            text_color: get_color_from_hex("#fcb711")
-            width: "350dp"
-            size_hint_x: None
-        FullImage:
-            source: "../src/imgs/at.png"
+        cols: 1
+        MDFloatLayout:
             
-        MDLabel:    
-            text: "Austria"
-            theme_text_color: "Custom"
-            font_style: "H6"
-            font_size: "22sp"
-            text_color: (1,1,1,1)  
-        MDLabel:
-            text: "sentnode19887294712394719phfndkashfk"
-            theme_text_color: "Custom"
-            font_style: "Subtitle2"
-            font_size: "14sp"
-            text_color: (0/255.0,141/255.0,155/255.0,255/255.0)
-        MDLabel:
-            text: " "
-            theme_text_color: "Custom"
-            font_style: "Subtitle2"
-            font_size: "14sp"
-            text_color: (0/255.0,141/255.0,155/255.0,255/255.0)
-        MDLabel:
-            text: "Vienna"
-            theme_text_color: "Custom"
-            font_style: "Subtitle2"
-            font_size: "14sp"
-            text_color: (0/255.0,141/255.0,155/255.0,255/255.0)                                    
-    MDGridLayout:
-        cols: 3
-        padding: 40,0,0,0
-        MDLabel:
-            text: "1000000udvpn/GB"
-            theme_text_color: "Custom"
-            font_style: "Subtitle2"
-            font_size: "14sp"
-            text_color: get_color_from_hex("#fcb711")
-        FullImage2:
-            source: "../src/imgs/fastavg.png"
-        MDBoxLayout:
-            padding: 80,30,0,0
             MDLabel:
-                text: "50.42 MB/s, 75.2 MB/s ."
+                text: "Schrodinger's Cat"
+                theme_text_color: "Custom"
+                font_style: "H6"
+                font_size: "24sp"
+                text_color: get_color_from_hex("#fcb711")
+                pos_hint: {"x" : 0, "top" : 1.3}
+                size_hint_x: None
+                width: "200dp"
+            Image:
+                source: "../src/imgs/at.png"
+                # {"x", "y", "top", "bottom", "left", "right" }
+                pos_hint: {"x" : .3, "top" : 1 }
+                
+            MDLabel:
+                text: "50.42 MB/s, 75.2 MB/s "
                 theme_text_color: "Custom"
                 font_style: "Subtitle2"
                 font_size: "12sp"
                 text_color: 1,1,1,1
+                pos_hint: {"x" : .42 , "top": .9 }
+            MDLabel:    
+                text: "Austria"
+                theme_text_color: "Custom"
+                font_style: "H6"
+                font_size: "22sp"
+                text_color: (1,1,1,1)
+                pos_hint: {"right" : 1.75 , "top": 0 }  
+            MDLabel:
+                text: "sentnode19887294712394719phfndkashfk"
+                theme_text_color: "Custom"
+                font_style: "Subtitle2"
+                font_size: "10sp"
+                text_color: (0/255.0,141/255.0,155/255.0,255/255.0)
+                pos_hint: {"x" : 0, "top" : .85}
+                               
+    MDGridLayout:
+        cols: 3
+        padding: 0,0,0,10
+        MDLabel:
+            text: "1000000udvpn/GB,10000utom,15000uosmo,10000000udec,10000uscrt"
+            theme_text_color: "Custom"
+            font_style: "Subtitle2"
+            font_size: "13sp"
+            text_color: get_color_from_hex("#fcb711")
+        Image:
+            source: "../src/imgs/fast.png"
+        MDBoxLayout:
+            padding: 80,30,0,0
+
                 
-
-
-
     MDRaisedButton:
         text: "Get Details"
         md_bg_color: get_color_from_hex("#fcb711")
@@ -107,7 +98,7 @@ Builder.load_string('''
     RecycleGridLayout:
         
         cols:1
-        default_size: dp(200), dp(180)
+        default_size: dp(200), dp(150)
         default_size_hint: 1, None
         size_hint_y: None
         height: self.minimum_height
@@ -124,7 +115,7 @@ class RecycleViewRow(MDCard):
 class MainScreen(RecycleView):    
     def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
-        self.data = [{'text': "Button " + str(x), 'id': str(x)} for x in range(100)]
+        self.data = [{'text': "Button " + str(x), 'id': str(x)} for x in range(10)]
     
 
     
