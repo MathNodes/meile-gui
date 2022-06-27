da3042918237064ad66841e3d33b40dad0b9bb15
freQniK <freQniK@protonmail.com>
2022-06-27 03:10:32 -0400

    Data stored as tree. Sub. algo improved. Many interface improvements. Refactor. Lots of additions. 5 star push.

--- tests/rv_country_swipe_drilldown.py
+++ tests/rv_country_swipe_drilldown.py
@@ -8,7 +8,7 @@ from kivymd.uix.card import MDCard, MDCardSwipe
 from kivy.uix.image import Image
 from kivy.uix.screenmanager import ScreenManager
 from kivy.uix.screenmanager import Screen, SlideTransition
-
+from kivymd.uix.behaviors import RoundedRectangularElevationBehavior
 
 Builder.load_string('''
 #:kivy 1.10.0
@@ -19,6 +19,7 @@ Builder.load_string('''
     
 <MainScreen>:
     name: "main"
+    md_bg_color: get_color_from_hex("#0d021b")
     MDLabel:
         text: "BLARG"
 
@@ -41,6 +42,12 @@ Builder.load_string('''
     
 <CountryRV>:
     viewclass: 'RecycleViewRow'
+    canvas.before:
+        Color:
+            rgba: get_color_from_hex("#fccf62")
+        Rectangle:
+            pos: self.pos
+            size: self.size
     RecycleGridLayout:
         
         cols:1
@@ -51,6 +58,12 @@ Builder.load_string('''
         orientation: 'lr-tb'
 <NodeRV>:
     viewclass: 'NodeViewRow'
+    canvas.before:
+        Color:
+            rgba: get_color_from_hex("#fccf62")
+        Rectangle:
+            pos: self.pos
+            size: self.size
     RecycleGridLayout:
         
         cols:1
@@ -59,13 +72,13 @@ Builder.load_string('''
         size_hint_y: None
         height: self.minimum_height
         orientation: 'lr-tb'
+        spacing: 15
+        padding: 4,4,4,4
+
         
 <RecycleViewRow>:
-    size_hint_y: None
-    height: self.height
-    type_swipe: "hand"
-
-
+    focus_behavior: True
+    padding: 10,10,10,10
     MDCardSwipeLayerBox:
         padding: "8dp"
 
@@ -75,7 +88,7 @@ Builder.load_string('''
             on_release: root.show_country_nodes(root.ids.country)
             
     MDCardSwipeFrontBox:
-        md_bg_color: get_color_from_hex("#0d021b")
+        md_bg_color: get_color_from_hex("#0f031d")
         MDGridLayout:
             cols: 1
             padding: 20,0,0,0
@@ -109,12 +122,15 @@ Builder.load_string('''
 
 
 <NodeViewRow>:
-    size_hint_y: None
+    
     pos_hint: {"center_x": .5, "center_y": .5}
     orientation: "vertical"
-    padding: 10
+    padding: 10,10
     border_radius: 20
-    elevation:0
+    radius: [10]
+    shadow_pos: 0,0
+    elevation: 42
+
     md_bg_color: get_color_from_hex("#0d021b")
     MDGridLayout:
         cols: 1
@@ -124,15 +140,29 @@ Builder.load_string('''
                 text: "Schrodinger's Cat"
                 theme_text_color: "Custom"
                 font_style: "H6"
-                font_size: "24sp"
+                font_size: "22sp"
                 text_color: get_color_from_hex("#fcb711")
                 pos_hint: {"x" : 0, "top" : 1.3}
                 size_hint_x: None
-                width: "200dp"
+                width: "400dp"
+            MDLabel:
+                text: "sentnode19887294712394719phfndkashfk"
+                theme_text_color: "Custom"
+                font_style: "Subtitle2"
+                font_size: "10sp"
+                text_color: (0/255.0,141/255.0,155/255.0,255/255.0)
+                pos_hint: {"x" : 0, "top" : 1.1}
+            MDLabel:
+                text: "1000000udvpn/GB,10000utom,15000uosmo,10000000udec,10000uscrt"
+                theme_text_color: "Custom"
+                font_style: "Subtitle2"
+                font_size: "13sp"
+                text_color: get_color_from_hex("#fcb711")
+                pos_hint: {"x" : 0, "top" : .85}
             Image:
                 source: "../src/imgs/at.png"
                 # {"x", "y", "top", "bottom", "left", "right" }
-                pos_hint: {"x" : .3, "top" : 1 }
+                pos_hint: {"x" : .3, "top" : 1.3 }
                 
             MDLabel:
                 text: "50.42 MB/s, 75.2 MB/s "
@@ -140,37 +170,22 @@ Builder.load_string('''
                 font_style: "Subtitle2"
                 font_size: "12sp"
                 text_color: 1,1,1,1
-                pos_hint: {"x" : .42 , "top": .9 }
+                pos_hint: {"x" : .42 , "top": 1.1 }
+            Image:
+                source: "../src/imgs/fast.png"
+                pos_hint: {"x" : .44 , "top": 1.1 }
+                size_hint: None, .45
             MDLabel:    
                 text: "Austria"
                 theme_text_color: "Custom"
                 font_style: "H6"
                 font_size: "22sp"
                 text_color: (1,1,1,1)
-                pos_hint: {"right" : 1.75 , "top": 0 }  
-            MDLabel:
-                text: "sentnode19887294712394719phfndkashfk"
-                theme_text_color: "Custom"
-                font_style: "Subtitle2"
-                font_size: "10sp"
-                text_color: (0/255.0,141/255.0,155/255.0,255/255.0)
-                pos_hint: {"x" : 0, "top" : .85}
-                               
-    MDGridLayout:
-        cols: 3
-        padding: 0,0,0,10
-        MDLabel:
-            text: "1000000udvpn/GB,10000utom,15000uosmo,10000000udec,10000uscrt"
-            theme_text_color: "Custom"
-            font_style: "Subtitle2"
-            font_size: "13sp"
-            text_color: get_color_from_hex("#fcb711")
-        Image:
-            source: "../src/imgs/fast.png"
-        MDBoxLayout:
-            padding: 80,30,0,0
+                pos_hint: {"right" : 1.75 , "top": .8 }  
 
-                
+                               
+            
+
     MDRaisedButton:
         text: "Get Details"
         md_bg_color: get_color_from_hex("#fcb711")
@@ -181,7 +196,7 @@ Builder.load_string('''
 ''')
     
 
-class NodeViewRow(MDCard):
+class NodeViewRow(MDCard,RoundedRectangularElevationBehavior):
     text = StringProperty()
 
 class WindowManager(ScreenManager):
