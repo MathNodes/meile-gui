f459662782c421e3b52ec69cd8d8a473b72d1a45
MathNodes <freQniK@mathnodes.com>
2022-07-03 00:23:21 -0400

    0.9.0-alpha.1 release for Linux

---
+++ pyinstaller.cmd
@@ -0,0 +1,3 @@
+# AMD64 Linux
+
+pyinstaller --onefile --add-data src/awoc/data/:data --add-data src/utils/fonts/:../utils/fonts --add-data src/utils/coinimg/:../utils/coinimg --add-data src/imgs/:../imgs --add-data src/kivy/:../kivy --add-data src/conf/config/:config meile-gui.py
