a4491ade55783767ec7315800a075fe09ff73635
MathNodes <freQniK@mathnodes.com>
2022-07-11 22:54:29 -0400

    v0.9.2-alpha.1 amnd64 linux

--- meile-gui.py
+++ meile-gui.py
@@ -2,17 +2,6 @@ import src.main.main as Meile
 import asyncio
 from threading import Thread
 
-
 if __name__ == "__main__":
     meilethread = Thread(target=Meile.app.run())
     meilethread.start()
-    
-
-
-
-
-
-    
-
-
-#Palette().run()
