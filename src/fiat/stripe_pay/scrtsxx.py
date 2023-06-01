9a655c9e8e28d6726b0a2d8a87ef5bb03785d4cc
MathNodes <freqnik@mathnodes.com>
2023-06-01 14:42:21 -0400

    Merge commits #4415ebd, #aae20b9

--- src/fiat/stripe_pay/scrtsxx.py
+++ src/fiat/stripe_pay/scrtsxx.py
@@ -1,8 +1,9 @@
-SECRET_KEY       = "sk_live_"
-WALLET_ADDRESS   = 'sent...'
-SERVER_ADDRESS   = "https://your-server.de:1111"
-API_ENDPOINT     = "/api/endpoint"
-USERNAME         = 'username'
-PASSWORD         = 'password'
-CCOMPAREAPI      = 'ccapi'
-CCOMPARE_API_URL = "https://min-api.cryptocompare.com/data/price?fsym=DVPN&tsyms=USD&extraParams=Meile"
+SECRET_KEY         = "sk_live_"
+WALLET_ADDRESS     = 'sent...'
+SERVER_ADDRESS     = "https://your-server.de:1111"
+API_ENDPOINT       = "/api/endpoint"
+MAX_SPEND_ENDPOINT = "/api/maxspend"
+USERNAME           = 'username'
+PASSWORD           = 'password'
+CCOMPAREAPI        = 'ccapi'
+CCOMPARE_API_URL   = "https://min-api.cryptocompare.com/data/price?fsym=DVPN&tsyms=USD&extraParams=Meile"
--- src/typedef/konstants.py
+++ src/typedef/konstants.py
@@ -44,7 +44,7 @@ class IBCTokens():
     
 class TextStrings():
     dash = "-"
-    VERSION = "v1.6.1"
+    VERSION = "v1.7.0"
     
 class MeileColors():
     DIALOG_BG_COLOR = "#121212"
