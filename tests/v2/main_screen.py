3f89598151660105075d7634791c1d0968fdd485
Alessandro Maggio <alex.tkd.alex@gmail.com>
2024-01-07 22:50:49 +0100

    Just copy the example of MDDataTable (https://kivymd.readthedocs.io/en/1.1.1/components/datatables/index.html)

--- tests/v2/main_screen.py
+++ tests/v2/main_screen.py
@@ -11,6 +11,8 @@ from kivy.metrics import dp
 
 from kivymd.uix.list import ImageLeftWidget
 
+from kivymd.uix.datatables import MDDataTable
+
 # meil uix from tests
 from uix.expansionpanel import MDExpansionPanelRoundIcon, MDExpansionPanelTwoLineSmall
 
@@ -143,12 +145,10 @@ WindowManager:
                         theme_text_color: "Custom"
                         text_color: "white"
 
-
-
-            MDFloatLayout:
+            AnchorLayout:
                 orientation: "horizontal"
                 md_bg_color: get_color_from_hex("#131313")
-
+                id: servers_datatable
 
 """
 
@@ -214,6 +214,126 @@ class MainScreen(Screen):
             )
             self.ids.countries_list.add_widget(item)
 
+
+        self.data_tables = MDDataTable(
+            use_pagination=True,
+            check=True,
+            column_data=[
+                ("No.", dp(30)),
+                ("Status", dp(30)),
+                ("Signal Name", dp(60), self.sort_on_signal),
+                ("Severity", dp(30)),
+                ("Stage", dp(30)),
+                ("Schedule", dp(30), self.sort_on_schedule),
+                ("Team Lead", dp(30), self.sort_on_team),
+            ],
+            row_data=[
+                (
+                    "1",
+                    ("alert", [255 / 256, 165 / 256, 0, 1], "No Signal"),
+                    "Astrid: NE shared managed",
+                    "Medium",
+                    "Triaged",
+                    "0:33",
+                    "Chase Nguyen",
+                ),
+                (
+                    "2",
+                    ("alert-circle", [1, 0, 0, 1], "Offline"),
+                    "Cosmo: prod shared ares",
+                    "Huge",
+                    "Triaged",
+                    "0:39",
+                    "Brie Furman",
+                ),
+                (
+                    "3",
+                    (
+                        "checkbox-marked-circle",
+                        [39 / 256, 174 / 256, 96 / 256, 1],
+                        "Online",
+                    ),
+                    "Phoenix: prod shared lyra-lists",
+                    "Minor",
+                    "Not Triaged",
+                    "3:12",
+                    "Jeremy lake",
+                ),
+                (
+                    "4",
+                    (
+                        "checkbox-marked-circle",
+                        [39 / 256, 174 / 256, 96 / 256, 1],
+                        "Online",
+                    ),
+                    "Sirius: NW prod shared locations",
+                    "Negligible",
+                    "Triaged",
+                    "13:18",
+                    "Angelica Howards",
+                ),
+                (
+                    "5",
+                    (
+                        "checkbox-marked-circle",
+                        [39 / 256, 174 / 256, 96 / 256, 1],
+                        "Online",
+                    ),
+                    "Sirius: prod independent account",
+                    "Negligible",
+                    "Triaged",
+                    "22:06",
+                    "Diane Okuma",
+                ),
+            ],
+            sorted_on="Schedule",
+            sorted_order="ASC",
+            elevation=2,
+        )
+        self.data_tables.bind(on_row_press=self.on_row_press)
+        self.data_tables.bind(on_check_press=self.on_check_press)
+
+        self.ids.servers_datatable.add_widget(self.data_tables)
+
+    def on_row_press(self, instance_table, instance_row):
+        '''Called when a table row is clicked.'''
+
+        print(instance_table, instance_row)
+
+    def on_check_press(self, instance_table, current_row):
+        '''Called when the check box in the table row is checked.'''
+
+        print(instance_table, current_row)
+
+    # Sorting Methods:
+    # since the https://github.com/kivymd/KivyMD/pull/914 request, the
+    # sorting method requires you to sort out the indexes of each data value
+    # for the support of selections.
+    #
+    # The most common method to do this is with the use of the builtin function
+    # zip and enumerate, see the example below for more info.
+    #
+    # The result given by these funcitons must be a list in the format of
+    # [Indexes, Sorted_Row_Data]
+
+    def sort_on_signal(self, data):
+        return zip(*sorted(enumerate(data), key=lambda l: l[1][2]))
+
+    def sort_on_schedule(self, data):
+        return zip(
+            *sorted(
+                enumerate(data),
+                key=lambda l: sum(
+                    [
+                        int(l[1][-2].split(":")[0]) * 60,
+                        int(l[1][-2].split(":")[1]),
+                    ]
+                ),
+            )
+        )
+
+    def sort_on_team(self, data):
+        return zip(*sorted(enumerate(data), key=lambda l: l[1][-1]))
 
 Builder.load_string(KV)
 
