d68741f58b979f3b87503259e3853dc56d08695a
Alessandro Maggio <alex.tkd.alex@gmail.com>
2024-01-11 14:51:59 +0100

    textsize is deprecated : DeprecationWarning: textsize is deprecated and will be removed in Pillow 10 (2023-07-01). Use textbbox or textlength instead.

--- src/utils/qr.py
+++ src/utils/qr.py
@@ -23,34 +23,34 @@ class QRCode():
 
     def generate_qr_code(self, ADDRESS):
         DepositCoin    = CoinsList.coins[2]
-        DepositAddress = ADDRESS 
-        
+        DepositAddress = ADDRESS
+
         coinLogo = self.MeileConfig.resource_path('../utils/coinimg/' + DepositCoin + '.png')
         logo = Image.open(coinLogo)
         basewidth = 100
-         
+
         # adjust image size
         wpercent = (basewidth/float(logo.size[0]))
         hsize = int((float(logo.size[1])*float(wpercent)))
         logo = logo.resize((basewidth, hsize))
-        
+
         QRcode = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
         QRcode.add_data(DepositAddress)
         QRcode.make()
 
         QRimg = QRcode.make_image(fill_color='Black', back_color="white").convert('RGB')
-         
+
         # set size of QR code
         pos = ((QRimg.size[0] - logo.size[0]) // 2,
                (QRimg.size[1] - logo.size[1]) // 2)
-        
+
         QRimg.paste(logo, pos)
-        
+
         # crop a bit
         border = (0, 4, 0, 30) # left, top, right, bottom
         QRimg = ImageOps.crop(QRimg, border)
-        
-        
+
+
         # Next Process is adding and centering the Deposit address on the image
         # Creating a background a little larger and pasting the QR
         # Image onto it with the text
@@ -60,15 +60,19 @@ class QRCode():
             fontSize = 12
         else:
             fontSize = 11
-            
+
         background = Image.new('RGBA', (QRimg.size[0], QRimg.size[1] + 15), (255,255,255,255))
         #robotoFont = ImageFont.truetype(pkg_resources.resource_filename(__name__, os.path.join('fonts', 'Roboto-BoldItalic.ttf')), fontSize)
         robotoFont = ImageFont.truetype(self.MeileConfig.resource_path('../utils/fonts/Roboto-BoldItalic.ttf'), fontSize)
-    
+
+        print("DepositAddress", DepositAddress)
+
         draw = ImageDraw.Draw(background)
-        w,h  = draw.textsize(DepositAddress)
+
+        # w,h  = draw.textsize(DepositAddress)  # Deprecated
+        w = draw.textlength(DepositAddress, font=robotoFont)
+
         draw.text(((QRimg.size[0]+15 - w)/2,QRimg.size[1]-2),DepositAddress, (0,0,0), font=robotoFont)
-        
+
         background.paste(QRimg, (0,0))
         background.save(path.join(self.IMGDIR, DepositCoin + ".png"))
-        
