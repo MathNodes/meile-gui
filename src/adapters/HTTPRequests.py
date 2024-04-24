95f74ef7e39450961985dab8d6c5a32b72691b5f
Tkd-Alex <alex.tkd.alex@gmail.com>
2024-04-24 15:02:13 +0200

    Follow: https://github.com/Tkd-Alex/meile-gui/commit/de3c263c755e7c81e2b72b9ca15d05ad0b92b5f1, method_whitelist was deprecated

--- src/adapters/HTTPRequests.py
+++ src/adapters/HTTPRequests.py
@@ -18,20 +18,35 @@ class TimeoutHTTPAdapter(HTTPAdapter):
         if timeout is None:
             kwargs["timeout"] = self.timeout
         return super().send(request, **kwargs)
-    
-    
+
+
 
 class MakeRequest():
     def __init__(self, TIMEOUT=DEFAULT_TIMEOUT, headers=None):
         self.headers = headers
         self.timeout = TIMEOUT
     def hadapter(self):
-        retries = Retry(
-            total=2,
-            status_forcelist=[429, 500, 502, 503, 504],
-            method_whitelist=["HEAD", "GET", "OPTIONS"]
-        )
-        #adapter = HTTPAdapter(max_retries=retries)
+        # https://github.com/Tkd-Alex/meile-gui/commit/de3c263c755e7c81e2b72b9ca15d05ad0b92b5f1
+        # https://github.com/urllib3/urllib3/blob/main/CHANGES.rst#1260-2020-11-10
+        # method_whitelist deprecated
+
+        # We could check the version if urllib3 but I don't want to do another import.
+
+        if 'DEFAULT_METHOD_WHITELIST' in dir(Retry):
+            retries = Retry(
+                total=2,
+                status_forcelist=[429, 500, 502, 503, 504],
+                method_whitelist=["HEAD", "GET", "OPTIONS"]
+            )
+        else:
+            # We are on >1.26.0 version, with allowed_methods (Retry.DEFAULT_ALLOWED_METHODS)
+            retries = Retry(
+                total=2,
+                status_forcelist=[429, 500, 502, 503, 504],
+                allowed_methods=["HEAD", "GET", "OPTIONS"]
+            )
+
+        # adapter = HTTPAdapter(max_retries=retries)
         requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
         http = requests.Session()
         http.headers = self.headers
