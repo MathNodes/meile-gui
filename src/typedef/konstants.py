5f19ae5706375b40f018e3cc7fd01497de9305b9
freqnik <freqnik@protonmail.com>
2024-10-15 22:39:28 -0400

    Add Indicator Color

--- src/typedef/konstants.py
+++ src/typedef/konstants.py
@@ -555,6 +555,7 @@ class MeileColors():
     MEILE                    = "#fcb711"
     DIALOG_BG_COLOR          = "#121212"
     DIALOG_BG_COLOR2         = "#181818"
+    INDICATOR                = "#00DD21"
     INACTIVE_DIALOG_BG_COLOR = "#50507c"
     ROW_HOVER                = "#39363c"
     FONT_FACE                = "../fonts/mplus-2c-bold.ttf"
