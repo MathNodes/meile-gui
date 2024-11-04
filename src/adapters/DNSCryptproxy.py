be63c4b55115849185bc2833540007f879fb852f
freqnik <freqnik@protonmail.com>
2024-11-04 01:16:04 -0500

    Merge #6ddc500 into 2.0

--- src/adapters/DNSCryptproxy.py
+++ src/adapters/DNSCryptproxy.py
@@ -7,6 +7,22 @@ from multiprocessing import Process
 from time import sleep
 
 class HandleDNSCryptProxy():
+    dnscrypt_start = """
+$dnscrypt_proxy_executable = "%s"
+$dnscrypt_proxy_config = "%s"
+
+Start-Process -FilePath $dnscrypt_proxy_executable -ArgumentList "-config `"$dnscrypt_proxy_config`"" -NoNewWindow -Wait
+"""
+    dnscrypt_stop = 'Stop-Process -Name "dnscrypt-proxy" -Force'
+    
+    dns_ps1 = """$interfaces = Get-NetAdapter | Where-Object { $_.Status -eq 'Up' }
+foreach ($interface in $interfaces) {
+    Set-DnsClientServerAddress -InterfaceIndex $interface.ifIndex -ServerAddresses %s
+    Set-DnsClientServerAddress -InterfaceIndex $interface.ifIndex -ServerAddresses %s
+}
+
+ Get-DnsClientServerAddress
+"""
     lh = "127.0.0.1"
     lhv6 = "::1"
     cf = "1.1.1.1"
@@ -20,7 +36,13 @@ class HandleDNSCryptProxy():
         pass
     
     def fork_dnscrypt(self):
-        dnscryptcmd = f"{self.dnscryptcmd}"
+        pltfrm = platform.system()
+        
+        if pltfrm == Arch.WINDOWS:
+            dnscryptcmd = f"gsudo.exe powershell.exe {self.dnscryptcmd}"   
+        else:
+            dnscryptcmd = f"{self.dnscryptcmd}"
+            
         process = subprocess.Popen(dnscryptcmd, shell=True,close_fds=True)
         print(f"DNSCryptProxy: {process.pid}")
         self.dnscrypt_pid = process.pid
@@ -28,10 +50,40 @@ class HandleDNSCryptProxy():
     # Be sure to run in thread or concurent.futures
     def dnscrypt(self, state: bool = False):
         chdir(ConfParams.BASEBINDIR)
-        dnscrypt_proxy_executable = path.join(ConfParams.BASEBINDIR, 'run_dnscrypt.sh')
         pltfrm = platform.system()
         if state == True:
-            if pltfrm == Arch.LINUX:
+            if pltfrm == Arch.WINDOWS:
+                dnscrypt_proxy_executable = path.join(ConfParams.BASEBINDIR, 'dnscrypt-proxy.exe')
+                dnscrypt_proxy_config = path.join(ConfParams.KEYRINGDIR, 'dnscrypt-proxy.toml')
+                dnscrypt_start = self.dnscrypt_start % (dnscrypt_proxy_executable, dnscrypt_proxy_config)
+                dns_bat = self.dns_ps1 % (self.lh, self.lhv6)
+                print(dns_bat)
+                print(dnscrypt_start)
+                #dnscryptcmd = f'"{dnscrypt_proxy_executable}" -config "{dnscrypt_proxy_config}"'
+                with open("change_dns.ps1", "w") as DNSFILE:
+                    DNSFILE.write(f"{dns_bat}\n")
+                    DNSFILE.write(f"{dnscrypt_start}")
+                    
+                
+                print("Startin DNScrypt-proxy...", end=' ')
+                self.dnscryptcmd = path.join(ConfParams.BASEBINDIR, "change_dns.ps1")
+                multiprocessing.get_context('spawn')
+                fork = Process(target=self.fork_dnscrypt)
+                fork.run()
+                sleep(1.5)
+
+            elif pltfrm == Arch.OSX:
+                dnscrypt_proxy_executable = path.join(ConfParams.BASEBINDIR, 'dnscrypt.sh')
+                dnscrypt_start = dnscrypt_proxy_executable + " up"
+                print("Startin DNScrypt-proxy...", end=' ')
+                self.dnscryptcmd = f"{dnscrypt_start}"
+                multiprocessing.get_context('spawn')
+                fork = Process(target=self.fork_dnscrypt)
+                fork.run()
+                sleep(10)
+                
+            else:
+                dnscrypt_proxy_executable = path.join(ConfParams.BASEBINDIR, 'run_dnscrypt.sh')
                 dnscrypt_start = dnscrypt_proxy_executable + " up"
                 print("Startin DNScrypt-proxy...", end=' ')
                 self.dnscryptcmd = f"pkexec sh -c '{dnscrypt_start}'"
@@ -39,10 +91,7 @@ class HandleDNSCryptProxy():
                 fork = Process(target=self.fork_dnscrypt)
                 fork.run()
                 sleep(1.5)
-                
-            # TODO: OS/X
-
-                
+            
                 
                 
         else:
@@ -70,7 +119,18 @@ class HandleDNSCryptProxy():
                 #process = subprocess.Popen(dnscmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 self.dnscrypt_pid = 0
                 
-            elif pltfrm == Arch.LINUX:
+            elif pltfrm == Arch.OSX:
+                dnscrypt_proxy_executable = path.join(ConfParams.BASEBINDIR, 'dnscrypt.sh')
+                dnscrypt_start = dnscrypt_proxy_executable + " down"
+                print("Stopping DNScrypt-proxy...", end=' ')
+                self.dnscryptcmd = f"{dnscrypt_start}"
+                multiprocessing.get_context('spawn')
+                fork = Process(target=self.fork_dnscrypt)
+                fork.run()
+                sleep(10)
+                
+            else:
+                dnscrypt_proxy_executable = path.join(ConfParams.BASEBINDIR, 'run_dnscrypt.sh')
                 dnscrypt_start = dnscrypt_proxy_executable + " down"
                 print("Stopping DNScrypt-proxy...", end=' ')
                 self.dnscryptcmd = f"pkexec sh -c '{dnscrypt_start}'"
@@ -78,7 +138,7 @@ class HandleDNSCryptProxy():
                 fork = Process(target=self.fork_dnscrypt)
                 fork.run()
                 sleep(1.5)
-            # TODO: OS/X 
+            # TODO: OS/X  
             
         chdir(ConfParams.KEYRINGDIR)
                     
