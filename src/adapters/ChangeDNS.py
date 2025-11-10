4e5c4d1ef0130f70e48d6c86cf8c4d96a119caa2
freqnik <freqnik@protonmail.com>
2025-11-10 15:30:53 -0500

    Fix DNS Adapter. #137

--- src/adapters/ChangeDNS.py
+++ src/adapters/ChangeDNS.py
@@ -2,6 +2,12 @@ import platform
 import re
 import psutil
 from os import path, chdir
+import tempfile
+import shlex
+import shutil
+import os
+import subprocess
+from pathlib import Path
 from subprocess import Popen, TimeoutExpired, PIPE
 from conf.meile_config import MeileGuiConfig
 
@@ -15,26 +21,83 @@ class ChangeDNS:
         pltfrm = platform.system()
 
         if pltfrm == "Linux":
-            resolv_file = path.join(MeileConfig.BASEDIR, "dns")
-            dns_file = open(resolv_file, "w")
 
-            dns_file.write(f"nameserver {self.dns}")
-            dns_file.flush()
-            dns_file.close()
+            info = platform.freedesktop_os_release()
+            print(info)
 
-            cmd = (
-                "pkexec bash -c 'cat %s | resolvconf -a wg99 && resolvconf -u'"
-                % resolv_file
-            )
+            if info.get("ID") == "debian":
+                DEBIAN = True
+            elif info.get("ID_LIKE") and "debian" in info["ID_LIKE"].split():
+                DEBIAN = True
+            else:
+                DEBIAN = False
+            
+            if not DEBIAN:
+                CONFIG = MeileConfig.read_configuration(MeileConfig.CONFFILE)
+                custom_dns = CONFIG['network']['dns']
+                resolv_file = path.join(MeileConfig.BASEDIR, "dns")
+                
+                dns_file = open(resolv_file, "w")
 
-            try:
-                proc = Popen(cmd, shell=True)
-                proc.wait(timeout=60)
-            except TimeoutExpired as e:
-                print(str(e))
+                dns_file.write(f"nameserver {custom_dns}")
+                dns_file.flush()
+                dns_file.close()
 
-            proc_out, proc_err = proc.communicate()
+                cmd = (
+                    "pkexec bash -c 'cat %s | resolvconf -a wg99 && resolvconf -u'"
+                    % resolv_file
+                )
 
+                try:
+                    proc = Popen(cmd, shell=True)
+                    proc.wait(timeout=60)
+                except TimeoutExpired as e:
+                    print(str(e))
+
+                proc_out, proc_err = proc.communicate()
+            
+            else:
+            
+                CONFIG = MeileConfig.read_configuration(MeileConfig.CONFFILE)
+                custom_dns = CONFIG['network']['dns']
+                interfaces = list(psutil.net_if_addrs().keys())
+                print("Interface names:", interfaces)
+                
+                LOOPBACK_NAMES = ['lo', 'lo0', 'Loopback Pseudo-Interface 1']
+                non_loopback_interfaces = [iface for iface in interfaces if iface not in LOOPBACK_NAMES]
+                
+                tmp = None
+                try:
+                    tmp = tempfile.NamedTemporaryFile(mode="w", encoding="utf-8", delete=False, prefix="set_dns_", suffix=".sh")
+                    script_path = Path(tmp.name)
+
+                    tmp.write("#!/usr/bin/env bash\n")
+                    tmp.write("set -euo pipefail\n\n")
+
+                    dns_quoted = shlex.quote(str(custom_dns))
+                    resolver_cmd = "systemd-resolve" if shutil.which("systemd-resolve") else "resolvectl"   
+                    for k, iface in enumerate(non_loopback_interfaces):
+                        iface_quoted = shlex.quote(iface)
+                        tmp.write(f"# iface[{k}] = {iface}\n")
+                        tmp.write(f"{resolver_cmd} --set-dns={dns_quoted} -i {iface_quoted}\n\n")
+
+                    tmp.flush()
+                    tmp.close()
+
+                    os.chmod(script_path, 0o700)
+
+                    subprocess.run(["pkexec", str(script_path)], check=True)
+
+                except subprocess.CalledProcessError as exc:
+                    print(f"Command failed: {exc}")
+                    return
+                finally:
+                    if tmp is not None:
+                        try:
+                            script_path.unlink(missing_ok=True)
+                        except Exception:
+                            pass
+                
         elif pltfrm == "Darwin":
             # sudo /usr/sbin/networksetup -listnetworkserviceorder
             # sudo /usr/sbin/networksetup -setdnsservers Wi-Fi 1.1.1.1
