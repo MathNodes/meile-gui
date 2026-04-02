216f40371a809da169030a586f898906690b8d0c
freQniK <freqnik@mathnodes.com>
2026-04-01 23:21:51 -0400

    hide the command window when connecting to v2ray node

--- src/cli/v2ray.py
+++ src/cli/v2ray.py
@@ -20,122 +20,134 @@ class V2RayHandler():
     v2rayproc    = "xray.exe"
     CREATE_NO_WINDOW = 0x08000000
     CREATE_NEW_CONSOLE = 0x00000010
-    
+    WINDOW_TITLE = "meile_v2ray_daemon"
+
     def __init__(self, script, **kwargs):
         self.v2ray_script = script
-
         print(self.v2ray_script)
-    
+
     def fork_v2ray(self):
-        v2ray_daemon_cmd = 'cmd.exe /c start cmd.exe /k gsudo.exe %s' %(self.v2ray_script)
-        #v2ray_daemon_cmd = 'gsudo.exe %s' %(self.v2ray_script)
-        v2ray_srvc_proc = Popen(v2ray_daemon_cmd, shell=True,stdout=PIPE,stderr=PIPE)
+        # Use "title" command so we can find the window
+        # reliably by its exact title
+        v2ray_daemon_cmd = (
+            'cmd.exe /c start "%s" cmd.exe /k gsudo.exe %s'
+            % (self.WINDOW_TITLE, self.v2ray_script)
+        )
+        v2ray_srvc_proc = Popen(
+            v2ray_daemon_cmd,
+            shell=True,
+            stdout=PIPE,
+            stderr=PIPE
+        )
         sleep(5)
         print("PID: %s" % v2ray_srvc_proc.pid)
-        hwnd = win32gui.FindWindow(None, f'C:\\Windows\\system32\\cmd.exe')
-        print(hwnd)
-        #win32gui.SetForegroundWindow(hwnd)
-        #hwnd = win32gui.GetForegroundWindow()
-        print(hwnd)
-        win32gui.ShowWindow(hwnd,win32con.SW_HIDE)
+
+        # Find and hide the window by our known title
+        hwnd = self._find_window_by_title(self.WINDOW_TITLE)
+        if hwnd:
+            print("Found window hwnd=%s, hiding..." % hwnd)
+            win32gui.ShowWindow(hwnd, win32con.SW_HIDE)
+        else:
+            print("WARNING: Could not find cmd window to hide")
+
         self.v2ray_pid = v2ray_srvc_proc.pid
 
-        
-    def enum_windows_callback(self, hwnd, wildcard):
-        if wildcard in win32gui.GetClassName(hwnd).lower():
-            win32gui.ShowWindow(hwnd, win32con.SW_HIDE)
-            
+    def _find_window_by_title(self, title):
+        """
+        Enumerate all windows and find the one whose
+        title contains our unique identifier.
+        """
+        result = []
+
+        def callback(hwnd, _):
+            window_title = win32gui.GetWindowText(hwnd)
+            if title in window_title:
+                result.append(hwnd)
+            return True
+
+        win32gui.EnumWindows(callback, None)
+
+        if result:
+            return result[0]
+        return None
+
     def start_daemon(self):
-        
+
         print("Starting v2ray service...")
-        
+
         routes_bat = 'routes.bat'
         gateways = netifaces.gateways()
-        
-        # This is a little tricky on windows, we may be able to use instead:
-        # One person complained not working on Ethernet, this may solve it:
+
         default_gateway = gateways[netifaces.AF_INET][0][0]
-        
-        #default_gateway = gateways['default'][netifaces.AF_INET][0]
-        
+
         SERVER = self.read_v2ray_config()
-        #wifidx = self.get_primary_if_idx(netifaces.gateways()['default'][netifaces.AF_INET][1])
-        
+
         batfile = open(routes_bat, 'w')
-        
-        #batfile.write('CD "%s"\n' % path.join(self.MeileConfig.BASEBINDIR, "V2Ray"))
+
         batfile.write('CD "%s"\n' % self.MeileConfig.BASEBINDIR)
         batfile.write('START "" /B %s run -c %s\n' % (self.v2rayproc, path.join(self.MeileConfig.BASEDIR, "v2ray_config.json")))
         batfile.write('timeout /t 1\n')
-        #batfile.write('CD "%s"\n' % self.MeileConfig.BASEBINDIR)
         batfile.write('START "" /B %s -device tun://tun00 -proxy socks5://127.0.0.1:1080"\n' % self.tunproc)
-        #v2ray = [path.join(self.MeileConfig.BASEBINDIR, "V2Ray", self.v2rayproc), "run", "-c",path.join(self.MeileConfig.BASEDIR, "v2ray_config.json")]
-        #Popen(v2ray, shell=False)
-        #sleep(3)
-        #tun2socks = [path.join(self.MeileConfig.BASEBINDIR, self.tunproc), "-device", "tun://tun00", "-proxy", "socks5://127.0.0.1:1080"]
-        #Popen(tun2socks, shell=False)
-        #sleep(5)
-        batfile.write('timeout /t 3\n')
+        batfile.write('timeout /t 2\n')
         batfile.write('netsh interface ip set address "tun00" static address=10.10.10.2 mask=255.255.255.0 gateway=10.10.10.1\n')
         batfile.write('netsh interface ip set dns name="tun00" static 1.1.1.1\n')
         batfile.write('route add %s %s metric 5\n' % (SERVER, default_gateway))
         batfile.write('route add 0.0.0.0 mask 0.0.0.0 10.10.10.1')
         batfile.flush()
         batfile.close()
-        
+
         self.v2ray_script = routes_bat
-        
-        #multiprocessing.get_context('spawn')
-        #fork = Process(target=self.fork_v2ray)
-        #fork.run()
+
         self.fork_v2ray()
-        
+        sleep(3)
+
         return True
-    
+
     def kill_daemon(self):
-        
+
         SERVER = self.read_v2ray_config()
         gateways = netifaces.gateways()
-        #print(gateways['default'][netifaces.AF_INET])
-        # This is a little tricky on windows, we may be able to use instead:
-        # One person complained not working on Ethernet, this may solve it:
         default_gateway = gateways[netifaces.AF_INET][0][0]
-        
-        #default_gateway = gateways['default'][netifaces.AF_INET][0]
-        #wifidx = self.get_primary_if_idx(netifaces.gateways()['default'][netifaces.AF_INET][1])
-        
+
         routes_bat = 'delroutes.bat'
-        
+
         batfile = open(routes_bat, 'w')
-        
+
         batfile.write('route delete %s %s metric 5\n' % (SERVER, default_gateway))
         batfile.write('route delete 0.0.0.0 mask 0.0.0.0 10.10.10.1\n')
         batfile.write('netsh interface set interface name="tun00" disable\n')
         batfile.write('timeout /t 3\n')
         batfile.write('TASKKILL /F /IM tun2socks.exe\n')
         batfile.write('TASKKILL /F /IM xray.exe\n')
-        batfile.write('TASKKILL /F /IM cmd.exe\n')
         batfile.flush()
         batfile.close()
-        
+
         self.v2ray_script = routes_bat
-        
-        v2ray_daemon_cmd = 'gsudo.exe %s' %(self.v2ray_script)
+
+        # Use our known title so we can find/kill it
+        v2ray_daemon_cmd = 'gsudo.exe %s' % (self.v2ray_script)
         proc2 = Popen(v2ray_daemon_cmd, shell=True)
         proc2.wait(timeout=30)
-        proc_out,proc_err = proc2.communicate()
+        proc_out, proc_err = proc2.communicate()
+
+        # Kill the hidden cmd.exe window we spawned
+        hwnd = self._find_window_by_title(self.WINDOW_TITLE)
+        if hwnd:
+            win32gui.PostMessage(
+                hwnd, win32con.WM_CLOSE, 0, 0
+            )
+
         return proc2.returncode
-        
+
     def read_v2ray_config(self):
-        
+
         with open(path.join(self.MeileConfig.BASEDIR, 'v2ray_config.json'), 'r') as V2RAYFILE:
             v2ray = V2RAYFILE.read()
-        
-        JSON = json.loads(v2ray)
-        
-        return JSON['outbounds'][0]['settings']['vnext'][0]['address']
-    
 
+        JSON = json.loads(v2ray)
+
+        return JSON['outbounds'][0]['settings']['vnext'][0]['address']
+        
 @dataclass
 class V2RayFragmentConfiguration:
     api_port: int
@@ -285,6 +297,7 @@ class V2RayFragmentConfiguration:
           },
               "stats": {}
         }
+
 
 @dataclass
 class V2RayConfiguration:
