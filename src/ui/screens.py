24b20d057ec51eebca3e2de6121f97c9c50b5d59
freqnik <freqnik@protonmail.com>
2026-05-13 19:52:22 -0400

    Add copy button for wallet. Re-design WalletScreen layout

--- src/ui/screens.py
+++ src/ui/screens.py
@@ -45,6 +45,7 @@ from kivy.uix.floatlayout import FloatLayout
 from kivymd.uix.label.label import MDLabel
 from kivy.animation import Animation
 from kivy.app import App
+from kivy.core.clipboard import Clipboard
 
 from requests.auth import HTTPBasicAuth
 import sys
@@ -1723,6 +1724,10 @@ class WalletScreen(Screen):
     def build(self, dt):
         Wallet = HandleWalletFunctions()
         self.SetBalances(Wallet.get_balance(self.ADDRESS))
+        
+    def copy_to_clipboard(self, text):
+        Clipboard.copy(text)
+        toast(text="Address copied!", duration=3.5)
 
     def refresh_wallet(self):
         self.build(None)
