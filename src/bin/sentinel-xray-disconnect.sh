4c96a49261a0902903e1021a2433952931c345f0
freqnik <freqnik@protonmail.com>
2026-06-16 23:17:54 -0400

    Update manual override scripts for v2ray

--- src/bin/sentinel-xray-disconnect.sh
+++ src/bin/sentinel-xray-disconnect.sh
@@ -5,7 +5,7 @@ PASSWORD="$2"
 osascript - "$CLICMD" "$PASSWORD"  <<EOF
 
     on run argv
-        do shell script ("launchctl bootout system /Library/LaunchDaemons/app.meile.xray.plist ; launchctl disable /Library/LaunchDaemons/app.meile.xray.plist ;  sleep 1 ; launchctl bootout system /Library/LaunchDaemons/app.meile.tun2socks.plist ; launchctl disable /Library/LaunchDaemons/app.meile.tun2socks.plist ; sleep 1 ; ${HOME}/.meile-gui/bin/routes.sh down") without altering line endings with administrator privileges        
+        do shell script ("launchctl bootout system /Library/Application Support/Meile/launchd/app.meile.xray.plist ; launchctl disable /Library/Application Support/Meile/launchd/app.meile.xray.plist ;  sleep 1 ; launchctl bootout system /Library/Application Support/Meile/launchd/app.meile.tun2socks.plist ; launchctl disable /Library/Application Support/Meile/launchd/app.meile.tun2socks.plist ; sleep 1 ; ${HOME}/.meile-gui/bin/routes.sh down") without altering line endings with administrator privileges        
     end run
 
 EOF
