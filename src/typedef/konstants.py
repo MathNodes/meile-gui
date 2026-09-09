bedef0d641b976c4b355282d835c099da1447d91
freQniK <freqnik@mathnodes.com>
2026-09-09 13:09:48 -0400

    Update precisioin for various assets on plan subs. UI improvements to plan and node sub dialog.

--- src/typedef/konstants.py
+++ src/typedef/konstants.py
@@ -543,9 +543,9 @@ class IBCTokens():
     IBCSCRT  = 'ibc/31FEE1A2A9F9C01113F90BD0BBCCE8FD6BBB8585FAF109A2101827DD1D5B95B8'
     IBCATOM  = 'ibc/A8C2D23A1E6F95DA4E48BA349667E322BD7A6C996D8A4AAE8BA72E190F3D1477'
     IBCDEC   = 'ibc/B1C0DDB14F25279A2026BC8794E12B259F8BDA546A3C5132CCAEE4431CE36783'
-    IBCOSMO  = 'ibc/ED07A3391A112B175915CD8FAF43A2DA8E4790EDE12566649D0C2F97716B8518'
-    IBCUNKWN = 'ibc/9BCB27203424535B6230D594553F1659C77EC173E36D9CF4759E7186EE747E84'
+    IBCOSMO  = 'ibc/ED07A3391A112B175915CD8FAF43A2DA8E4790EDE12566649D0C2F97716B8518' 
     IBCNAM   = 'ibc/700A05A54B68723DAD9A7C97B39C5DDAB5E34B621A3400482C737E54456BDCD7'
+    IBCUNKWN = 'ibc/9BCB27203424535B6230D594553F1659C77EC173E36D9CF4759E7186EE747E84'
     
     ZANO_WALLET = "ZxDEXCMwvpwirJujwjZExHfKFEqNjMWdjLw5MGYXa5KxScvcsr2BNGm8TFyk4mvFAhAPtWh54tadWNgS5CxPAqXX1fpoQLkJJ"
     
@@ -602,6 +602,44 @@ class IBCTokens():
                     'ethx' : 'ethereum',
                     'solx' : 'solana',
                     'tonx' : 'the-open-network'}
+    ZANO_ASSETS = {
+        'zano': {
+            'asset_id': 'd6329b5b1f7c0805b5c345f4957554002a2f557845f64d7645dae0e051a6498a',
+            'divisor': 10 ** 12,
+        },
+        'fusd': {
+            'asset_id': '86143388bd056a8f0bab669f78f14873fac8e2dd8d57898cdb725a2d5e2e4f8f',
+            'divisor': 10 ** 4,
+        },
+        'bchx': {
+            'asset_id': '3de9ad7243afa49e0ade6839e97a9f10a527c4958ece2fc9cb1b87a44032167d',
+            'divisor': 10 ** 8,
+        },
+        'bnbx': {
+            'asset_id': '6ca3fa07f1b6a75b6e195d2918c32228765968b54ea691c75958affa1c4073fb',
+            'divisor': 10 ** 6,
+        },
+        'btcx': {
+            'asset_id': '040a180aca4194a158c17945dd115db42086f6f074c1f77838621a4927fffa91',
+            'divisor': 10 ** 8,
+        },
+        'daix': {
+            'asset_id': '24819c4b65786c3ac424e05d9ef4ab212de6222cc73bc5c4b012df5a3107eea4',
+            'divisor': 10 ** 6,
+        },
+        'ethx': {
+            'asset_id': '93da681503353509367e241cda3234299dedbbad9ec851de31e900490807bf0c',
+            'divisor': 10 ** 6,
+        },
+        'solx': {
+            'asset_id': '65b3bc549c8bc2c773781d5436f25f7af84644e61baaabd675d9867b007d17b4',
+            'divisor': 10 ** 6,
+        },
+        'tonx': {
+            'asset_id': 'bfa6609a94e39f418d9adb000f89edc7bd180fd120f1cd272201220e3070fb4f',
+            'divisor': 10 ** 6,
+        },
+    }
     
     NOWCOINS     = ["firo", "beam", "pivx", "zec", "dcr", "epic",
                     "bch", "kmd", "xno", "sol", "dgb", "xaut", "eth",
@@ -612,8 +650,8 @@ class IBCTokens():
     #mu_coins     = ["tsent", "udvpn", "uscrt", "uosmo", "uatom", "udec"]
 class TextStrings():
     dash = "-"
-    VERSION           = "v2.7.0"
-    BUILD             = "17877851293"
+    VERSION = "v2.7.1"
+    BUILD = "1788953081370"
     RootTag           = "SENTINEL"
     WG_HASH           = ""
     WG_QUICK_HASH     = ""
@@ -680,4 +718,3 @@ class NodeKeys():
     NodeVersions  = [str(item).zfill(3) for item in range(70,1000)]
     Nodetypes = ['residential', 'business', 'hosting', 'edu']
     ProtocolTypes = ["WireGuard", "V2Ray", "OpenVPN", "XRay", "AmneziaWG", "Hysteria2"]
-
--- src/ui/widgets.py
+++ src/ui/widgets.py
@@ -325,7 +325,7 @@ class SubscribeContent(BoxLayout):
                 "text": f"{i}",
                 "height": dp(56),
                 "on_release": lambda x=f"{i}": self.set_item(x),
-            } for i in IBCTokens.ibc_coins #+ ['xmr']
+            } for i in IBCTokens.ibc_coins[:-1] #+ ['xmr']
         ]
         self.menu = MDDropdownMenu(
             caller=self.ids.drop_item,
@@ -336,6 +336,7 @@ class SubscribeContent(BoxLayout):
         )
         self.menu.bind()
         self.ids.drop_item.current_item = IBCTokens.ibc_coins[0]
+        self.ids.price.text = self.price_text.replace(' ', '\n').rstrip()
         self.parse_coin_deposit(self.ids.drop_item.current_item)
         self.build()
         
@@ -519,7 +520,7 @@ class PlanSubscribeContent(BoxLayout):
                 "text": f"{i}",
                 "height": dp(56),
                 "on_release": lambda x=f"{i}": self.set_item(x),
-            } for i in IBCTokens.ibc_coins
+            } for i in IBCTokens.ibc_coins[:-1]
         ]
         self.menu = MDDropdownMenu(
             caller=self.ids.drop_item,
@@ -590,7 +591,7 @@ class PlanSubscribeContent(BoxLayout):
                         "text": f"{i}",
                         "height": dp(56),
                         "on_release": lambda x=f"{i}": self.set_item(x),
-                    } for i in IBCTokens.ibc_coins
+                    } for i in IBCTokens.ibc_coins[:-1]
                 ]
                 self.menu.items = menu_items
                 self.set_item(IBCTokens.ibc_coins[0])
@@ -636,13 +637,21 @@ class PlanSubscribeContent(BoxLayout):
             if mu_coin == "p2p":
                 value = float(price.strip())
             else:
-                value = round(
-                    float(price.strip()) *
-                    self.price_cache["p2p"]["price"] /
-                    self.price_cache[mu_coin]["price"], 8
-                )
-    
-            deposit_text = str(format(round(month * value, 8), '8f'))
+                try:
+                    value = round(
+                        float(price.strip()) *
+                        self.price_cache["p2p"]["price"] /
+                        round(self.price_cache[mu_coin]["price"],8), 8
+                    )
+                except:
+                    value = 0
+            if "btc" in mu_coin and value != 0:
+                deposit_text = f"{month * value:.8f}"
+            else:
+                if value != 0:
+                    deposit_text = f"{month * value:.5f}"
+                else:
+                    deposit_text = 0
             self.ids.deposit.text = deposit_text
     
             if self.deposit_callback:
@@ -1343,7 +1352,11 @@ class PlanRow(MDGridLayout):
     
                 # Generate QR Code
                 QRcode = QRCode()
-                self.invoice_content.ids.qr_img.source = QRcode.generate_qr_code(zaddress, mu_coin) 
+                if mu_coin == "arrr":
+                    coin_uri = f"pirate:{zaddress}?amount={total_arrr}"
+                else:
+                    coin_uri = f"{mu_coin}:{zaddress}?amount={total_arrr}"
+                self.invoice_content.ids.qr_img.source = QRcode.generate_qr_code(coin_uri, mu_coin) 
     
                 self.dialog = MDDialog(
                     title="Waiting for invoice to be paid...",
@@ -1383,7 +1396,8 @@ class PlanRow(MDGridLayout):
     
                 # Generate QR Code
                 QRcode = QRCode()
-                self.invoice_content.ids.qr_img.source = QRcode.generate_qr_code(zaddress, "firo") 
+                coin_uri = f"{mu_coin}:{zaddress}?amount={total_arrr}"
+                self.invoice_content.ids.qr_img.source = QRcode.generate_qr_code(coin_uri, "firo") 
     
                 self.dialog = MDDialog(
                     title="Waiting for invoice to be paid...",
@@ -1403,13 +1417,20 @@ class PlanRow(MDGridLayout):
                 yield 0.6
                 self.start_payment_thread_firo(total_arrr)
                 
-            elif mu_coin in ["zano", "fusd","bchx", "bnbx", "btcx", "daix", "ethx", "solx", "tonx"]:
+            elif mu_coin in ["zano", "fusd", "bchx", "bnbx", "btcx", "daix", "ethx", "solx", "tonx"]:
                 zaddress = IBCTokens.ZANO_WALLET
                 
                 price_api = GetPriceAPI()
                 arrrusd = price_api.get_usd(mu_coin)
                 cost = usd*ConfParams.BTCPAYADJ
-                total_arrr = round(float(cost) / float(arrrusd['price']),4)
+                if mu_coin in ["zano", "ton"]:
+                    total_arrr = round(float(cost) / float(arrrusd['price']),4)
+                elif mu_coin in ["fusd", "daix"]:
+                    total_arrr = round(float(cost) / float(arrrusd['price']),2)
+                elif mu_coin in ["bchx", "bnbx", "ethx", "solx"]:
+                    total_arrr = round(float(cost) / float(arrrusd['price']),6)
+                else:
+                    total_arrr = round(float(cost) / float(arrrusd['price']),8)
                 
                 if self.dialog:
                     self.dialog.dismiss()
@@ -1423,7 +1444,9 @@ class PlanRow(MDGridLayout):
     
                 # Generate QR Code
                 QRcode = QRCode()
-                self.invoice_content.ids.qr_img.source = QRcode.generate_qr_code(zaddress, mu_coin) 
+                aid = IBCTokens.ZANO_ASSETS[mu_coin]['asset_id']
+                coin_uri = f"zano:{zaddress}?asset_id={aid}&tx_amount={total_arrr}&message={self.ADDRESS}"
+                self.invoice_content.ids.qr_img.source = QRcode.generate_qr_code(coin_uri, mu_coin) 
     
                 self.dialog = MDDialog(
                     title="Waiting for invoice to be paid...",
@@ -1462,7 +1485,8 @@ class PlanRow(MDGridLayout):
     
                 # Generate QR Code
                 QRcode = QRCode()
-                self.invoice_content.ids.qr_img.source = QRcode.generate_qr_code(zaddress, mu_coin) 
+                coin_uri = f"zephyr:{zaddress}?tx_amount={total_arrr}"
+                self.invoice_content.ids.qr_img.source = QRcode.generate_qr_code(coin_uri, mu_coin) 
     
                 self.dialog = MDDialog(
                     title="Waiting for invoice to be paid...",
